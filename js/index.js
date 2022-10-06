828ded4f0f868d71e804bfaefb536165b2c663d7
Tomas Riat <tomasiriat@hotmail.com>
2022-10-05 21:59:40 -0300

    modif

--- js/index.js
+++ js/index.js
@@ -39,45 +39,45 @@ botonComprar.addEventListener('click', () => {
 })
 
 
-stockProductos.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML = `
-        <div class="card">
-        <p>${producto.nombre}</p>
-                <div>
-                    <img class="imgProducto" src=${producto.img} alt="foto del producto"/>
-                </div>
-                <div>
-                    <p class="precioProducto">Precio:$${producto.precio}</p>
-                </div>
-                <div class="btn-container">
-                    <button id= "agregar${producto.id}" class="btnAgregar">add to cart</button>
-                </div>
-            </div>
-    `
-    contenedorProductos.appendChild(div)
+// stockProductos.forEach((producto) => {
+//     const div = document.createElement('div')
+//     div.classList.add('producto')
+//     div.innerHTML = `
+//         <div class="card">
+//         <p>${producto.nombre}</p>
+//                 <div>
+//                     <img class="imgProducto" src=${producto.img} alt="foto del producto"/>
+//                 </div>
+//                 <div>
+//                     <p class="precioProducto">Precio:$${producto.precio}</p>
+//                 </div>
+//                 <div class="btn-container">
+//                     <button id= "agregar${producto.id}" class="btnAgregar">add to cart</button>
+//                 </div>
+//             </div>
+//     `
+//     contenedorProductos.appendChild(div)
 
 
-    const boton = document.getElementById(`agregar${producto.id}`)
+//     const boton = document.getElementById(`agregar${producto.id}`)
 
-    boton.addEventListener('click', () => {
+//     boton.addEventListener('click', () => {
 
-        agregarAlCarrito(producto.id)
+//         agregarAlCarrito(producto.id)
         
 
-Toastify({
+// Toastify({
 
-    text: "Agregado al Carrito",
-    gravity: "bottom",
-    duration: 3000,
-    style: {
-        background: "#333133",
-    },
-    }).showToast();
+//     text: "Agregado al Carrito",
+//     gravity: "bottom",
+//     duration: 3000,
+//     style: {
+//         background: "#333133",
+//     },
+//     }).showToast();
     
-    })
-})
+//     })
+// })
 
 
 const agregarAlCarrito = (prodId) => {
--- js/stock.js
+++ js/stock.js
@@ -1,12 +1,47 @@
+const botonProductos = document.getElementById('botonProductos')
 
-let stockProductos = []
 
-function cargarJSON(){
-    fetch('../productos.json')
-        .then(response => response.json)
-        .then(data => data.json)
-        return 
+botonProductos.onclick = async () => {
+    const infoProducto = await fetch('../productos.json')
+    const infoProductoJson = await infoProducto.json()
+    const stockProductos = infoProductoJson.results
+    console.log(stockProductos)
+    stockProductos.forEach(producto => {
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+            <div class="card">
+            <p>${producto.nombre}</p>
+                    <div>
+                        <img class="imgProducto" src=${producto.img} alt="foto del producto"/>
+                    </div>
+                    <div>
+                        <p class="precioProducto">Precio:$${producto.precio}</p>
+                    </div>
+                    <div class="btn-container">
+                        <button id= "agregar${producto.id}" class="btnAgregar">add to cart</button>
+                    </div>
+                </div>
+        `
+        contenedorProductos.appendChild(div)
+
+        const boton = document.getElementById(`agregar${producto.id}`)
+
+    boton.addEventListener('click', () => {
+
+        agregarAlCarrito(producto.id)
+        
+
+    Toastify({
+
+        text: "Agregado al Carrito",
+        gravity: "bottom",
+        duration: 3000,
+        style: {
+            background: "#333133",
+        },
+        }).showToast();
+        
+        })
+    })
 }
-
-cargarJSON()
-console.log(cargarJSON)
